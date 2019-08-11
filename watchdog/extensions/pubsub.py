9c5e29676566a15ef6db78d56b32bc3ba62c5159
Robert Fasano <robert.j.fasano@colorado.edu>
2019-08-11 11:41:20 -0600

    Use either floats or dataframes depending on context

--- watchdog/extensions/pubsub.py
+++ watchdog/extensions/pubsub.py
@@ -7,8 +7,11 @@ class Publisher:
         self.socket = zmq.Context().socket(zmq.PUB)
         self.socket.bind(f"tcp://{address}:{port}")
 
-    def update(self, dataframe):
-        self.socket.send_string(json.dumps(dataframe.to_json()))
+    def update(self, data):
+        if isinstance(data, pd.DataFrame):
+            self.socket.send_string(json.dumps(dataframe.to_json()))
+        else:
+            self.socket.send_string(str(data))
 
 class Subscriber:
     def __init__(self, address='127.0.0.1', port=1105):
@@ -16,5 +19,12 @@ class Subscriber:
         self.socket.setsockopt_string(zmq.SUBSCRIBE, '')
         self.socket.connect(f"tcp://{address}:{port}")
 
-    def get(self):
-        return pd.read_json(json.loads(self.socket.recv_string()))
+    def receive(self):
+        ''' Receives either a float or json-formatted dataframe and processes and
+            returns it by context.
+        '''
+        msg = self.socket.recv_string()
+        try:
+            return float(msg)
+        except TypeError:
+            return pd.read_json(msg)
