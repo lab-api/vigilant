a4a30b89f341c0efd15bb8e7d703fd9025f30456
Robert Fasano <robert.j.fasano@colorado.edu>
2020-03-09 17:13:44 -0600

    Change InfluxClient arguments for generality

--- vigilant/extensions/influx.py
+++ vigilant/extensions/influx.py
@@ -2,14 +2,15 @@ from influxdb import DataFrameClient
 import pandas as pd
 
 class InfluxClient:
-    def __init__(self, address='localhost:8086', database='demo'):
+    def __init__(self, database, measurement, address='localhost:8086', username='root', password='root'):
         ip = address.split(':')[0]
         port = int(address.split(':')[1])
-        self.client = DataFrameClient(ip, port, 'root', 'root', database)
         self.database = database
+        self.measurement = measurement
+        self.client = DataFrameClient(ip, port, username, password, self.database)
 
-        if not self.database_exists(database):
-            self.client.create_database(database)
+        if not self.database_exists(self.database):
+            self.client.create_database(self.database)
 
     def database_exists(self, name):
         for db in self.client.get_list_database():
@@ -19,7 +20,7 @@ class InfluxClient:
 
     def update(self, data):
         data.index = pd.to_datetime(data.index)
-        self.client.write_points(data, self.database, protocol='json')
+        self.client.write_points(data, self.measurement, protocol='json')
 
     def read(self):
-        return self.client.query(f'select * from {self.database}')[self.database]
+        return self.client.query(f'select * from {self.measurement}')[self.measurement]
