73e11addf4f2a9cca8a1705b673596343de6d5be
Robert Fasano <robert.j.fasano@colorado.edu>
2020-03-09 21:48:14 -0600

    Use Influx HTTP API instead of influxdb package to avoid mysterious data persistence issues

--- vigilant/extensions/influx.py
+++ vigilant/extensions/influx.py
@@ -1,26 +1,45 @@
-from influxdb import DataFrameClient
 import pandas as pd
+import requests
 
 class InfluxClient:
-    def __init__(self, database, measurement, address='localhost:8086', username='root', password='root'):
-        ip = address.split(':')[0]
-        port = int(address.split(':')[1])
+    def __init__(self, database, measurement, addr='localhost', port=8086):
+        self.address = f'http://{addr}:{port}'
         self.database = database
         self.measurement = measurement
-        self.client = DataFrameClient(ip, port, username, password, self.database)
 
-        if not self.database_exists(self.database):
-            self.client.create_database(self.database)
+        if not self.database in self.list_databases():
+            self.create_database(self.database)
 
-    def database_exists(self, name):
-        for db in self.client.get_list_database():
-            if db['name'] == name:
-                return True
-        return False
+    def create_database(self, name):
+        params = {'q': f'CREATE DATABASE "{name}"'}
+        requests.post(f'{self.address}/query', params=params)
+
+    def list_databases(self):
+        params = {'q': "SHOW DATABASES"}
+        r = requests.get(f'{self.address}/query', params=params).json()
+        dbs = r['results'][0]['series'][0]['values']
+        return [x[0] for x in dbs]
 
     def update(self, data):
+        ''' Writes a set of points to the database '''
         data.index = pd.to_datetime(data.index)
-        self.client.write_points(data, self.measurement, protocol='json')
+
+        for i in range(len(data)):
+            payload = f'{self.measurement} '
+            field_values = []
+
+            timestamp = int(data.index[i].timestamp())
+            for column in data.columns:
+                field_values.append(f'{column}={data.iloc[i][column]}')
+
+            payload += ','.join(field_values)
+            payload += f' {timestamp}'
+            requests.post(f"{self.address}/write?db={self.database}&precision=s",
+                          data=payload.encode('utf-8'))
 
     def read(self):
-        return self.client.query(f'select * from {self.measurement}')[self.measurement]
+        params = {'q': f'select * from {self.database}..{self.measurement}'}
+        r = requests.get(f'{self.address}/query', params=params).json()
+        r = r['results'][0]['series'][0]
+
+        return pd.DataFrame(r['values'], columns=r['columns']).set_index('time')
